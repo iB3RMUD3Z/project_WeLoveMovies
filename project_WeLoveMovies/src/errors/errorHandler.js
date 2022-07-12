d473724bfd119f06dbb5e556871557bc01681096
Ivan Bermudez <94328269+iB3RMUD3Z@users.noreply.github.com>
2022-07-12 16:13:09 -0500

    Delete project_WeLoveMovies directory

--- project_WeLoveMovies/src/errors/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(error, _request, response, _next) {
-  const { status = 500, message = "Something went wrong!" } = error;
-  response.status(status).json({ error: message });
-}
-
-module.exports = errorHandler;
